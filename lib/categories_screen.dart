1ab0aa67ef8e3fa4243939aea72eeafe630f944b
Thao Pham Van <phamthaobk94@gmail.com>
2020-12-02 13:27:59 +0700

    create Deli-Meal app project

---
+++ lib/categories_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/category_item.dart';
+import 'dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('DeliMeals'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(15),
+        child: GridView(
+          children: DUMMY_CATEGORIES
+              .map((catData) => CategoryItem(catData.title, catData.color))
+              .toList(),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20),
+        ),
+      ),
+    );
+  }
+}
